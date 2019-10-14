1f3a5719254a6d1685bd923efc1741a53225b7f7
romankalab <roan.kalab@gmail.com>
2019-10-14 08:09:19 +0200

    Initial

---
+++ Open-Lab-01.5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Open_Lab_01._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BoolReverse(true));
+        }
+        private static bool BoolReverse(bool boolean)
+        {
+            return !boolean;
+        }
+    }
+}
